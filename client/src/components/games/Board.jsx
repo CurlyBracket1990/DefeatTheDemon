c7b16c7f9fecae0c91dfb4e7f301e3fe9ca97798
CurlyBracket1990 <stefan_moes@live.nl>
2019-03-26 15:01:28 +0100

    First commit

--- client/src/components/games/Board.jsx
+++ client/src/components/games/Board.jsx
@@ -2,13 +2,35 @@ import React from 'react'
 import './Board.css'
 
 const renderCel = (makeMove, rowIndex, cellIndex, symbol, hasTurn) => {
+  if(rowIndex === 3 && cellIndex === 0){
+    return (
+      <button
+        className="board-tile"
+        disabled={hasTurn}
+        onClick={() => makeMove(rowIndex, cellIndex)}
+        key={`${rowIndex}-${cellIndex}`}
+      >{symbol || 'x'}</button>
+    )
+  }
+
+  if(rowIndex === 3 && cellIndex === 3){
+    return (
+      <button
+        className="board-tile"
+        disabled={hasTurn}
+        onClick={() => makeMove(rowIndex, cellIndex)}
+        key={`${rowIndex}-${cellIndex}`}
+      >{symbol || 'y'}</button>
+    )
+  }
+
   return (
     <button
       className="board-tile"
       disabled={hasTurn}
       onClick={() => makeMove(rowIndex, cellIndex)}
       key={`${rowIndex}-${cellIndex}`}
-    >{symbol || '-'}</button>
+    >{symbol || ''}</button>
   )
 }
 
